c67522fd72c3a68d9018af66139c87c2c358f55e
Adil <adil.nawaz@siparadigm.com>
2025-12-10 20:15:09 +0500

    build

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/keepclose-your-connection-hub">
+      <BrowserRouter basename="/keepclose">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "development" ? "/" : "/keepclose-your-connection-hub/",
+  base: mode === "development" ? "/" : "/keepclose/",
   server: {
     host: "::",
     port: 8080,
